81d2fbcfd618c3cefd5b6a51f28e3a7e8bdd5b73
Connor Farrell <connorfarrell7@gmail.com>
2021-10-26 17:45:09 -0400

    added templates and index.html for Flask

--- Scraping.py
+++ Scraping.py
@@ -1,11 +1,29 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
+### Scrape all info
+def scrape_all():
+    
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 ### Scrape the title and summary of the first story
 def mars_news(browser):
@@ -66,4 +84,7 @@ def mars_facts():
 
     return df.to_html()
 
-browser.quit()
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
